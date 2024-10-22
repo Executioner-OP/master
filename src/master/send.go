4e5b0af0bbf7606177a76d8c3e13b117fccdbc11
shrey.bhadja <shrey.bhadja@vijigi.com>
2024-10-22 23:06:41 +0530

    Implement RabbitMQ connection and Fiber API server with message publishing

--- src/master/send.go
+++ src/master/send.go
@@ -3,17 +3,73 @@ package main
 import (
 	"log"
 
+	fiber "github.com/gofiber/fiber/v2"
+	logger "github.com/gofiber/fiber/v2/middleware/logger"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-func failOnError(err error, msg string) {
-	if err != nil {
-		log.Panicf("%s: %s", msg, err)
-	}
-}
-
 func main() {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
+
+	// Create a new RabbitMQ connection.
+	connectRabbitMQ, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		panic(err)
+	}
+	defer connectRabbitMQ.Close()
+
+	// Let's start by opening a channel to our RabbitMQ
+	// instance over the connection we have already
+	// established.
+	channelRabbitMQ, err := connectRabbitMQ.Channel()
+	if err != nil {
+		panic(err)
+	}
+	defer channelRabbitMQ.Close()
+
+	// With the instance and declare Queues that we can
+	// publish and subscribe to.
+	_, err = channelRabbitMQ.QueueDeclare(
+		"QueueService1", // queue name
+		true,            // durable
+		false,           // auto delete
+		false,           // exclusive
+		false,           // no wait
+		nil,             // arguments
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// Create a new Fiber instance.
+	app := fiber.New()
+
+	// Add middleware.
+	app.Use(
+		logger.New(), // add simple logger
+	)
+
+	// Add route.
+	app.Get("/send", func(c *fiber.Ctx) error {
+		// Create a message to publish.
+		message := amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(c.Query("msg")),
+		}
+
+		// Attempt to publish a message to the queue.
+		if err := channelRabbitMQ.Publish(
+			"",              // exchange
+			"QueueService1", // queue name
+			false,           // mandatory
+			false,           // immediate
+			message,         // message to publish
+		); err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	// Start Fiber API server.
+	log.Fatal(app.Listen(":3000"))
 }
