285fb9cd986020576c01eeaa9fc663734c35a07d
Shreypatel13ll <shrey1patel2@gmail.com>
2024-10-24 11:38:05 +0530

    basic queue test RabbitMQ

--- src/master/send.go
+++ src/master/send.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"log"
 	"strconv"
 	"time"
@@ -15,16 +14,6 @@ func failOnError(err error, msg string) {
 	}
 }
 
-func fib(n int) int {
-	if n == 0 {
-		return 0
-	} else if n == 1 {
-		return 1
-	} else {
-		return fib(n-1) + fib(n-2)
-	}
-}
-
 func main() {
 	conn, err := amqp.Dial("amqp://admin:ello@206.189.131.249:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
@@ -34,62 +23,32 @@ func main() {
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
+	// Declare a queue for the tasks
 	q, err := ch.QueueDeclare(
-		"rpc_queue", // name
-		false,       // durable
-		false,       // delete when unused
-		false,       // exclusive
-		false,       // no-wait
-		nil,         // arguments
+		"task_queue", // name of the queue
+		true,         // durable
+		false,        // delete when unused
+		false,        // exclusive
+		false,        // no-wait
+		nil,          // arguments
 	)
 	failOnError(err, "Failed to declare a queue")
 
-	err = ch.Qos(
-		1,     // prefetch count
-		0,     // prefetch size
-		false, // global
-	)
-	failOnError(err, "Failed to set QoS")
-
-	msgs, err := ch.Consume(
-		q.Name, // queue
-		"",     // consumer
-		false,  // auto-ack
-		false,  // exclusive
-		false,  // no-local
-		false,  // no-wait
-		nil,    // args
-	)
-	failOnError(err, "Failed to register a consumer")
-
-	var forever chan struct{}
-
-	go func() {
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-		for d := range msgs {
-			n, err := strconv.Atoi(string(d.Body))
-			failOnError(err, "Failed to convert body to integer")
-
-			log.Printf(" [.] fib(%d)", n)
-			response := fib(n)
-
-			err = ch.PublishWithContext(ctx,
-				"",        // exchange
-				d.ReplyTo, // routing key
-				false,     // mandatory
-				false,     // immediate
-				amqp.Publishing{
-					ContentType:   "text/plain",
-					CorrelationId: d.CorrelationId,
-					Body:          []byte(strconv.Itoa(response)),
-				})
-			failOnError(err, "Failed to publish a message")
-
-			d.Ack(false)
-		}
-	}()
-
-	log.Printf(" [*] Awaiting RPC requests")
-	<-forever
+	// Push some IDs into the queue as tasks
+	for i := 1; i <= 10; i++ {
+		body := strconv.Itoa(i) // Convert the ID to string
+		err = ch.Publish(
+			"",     // exchange
+			q.Name, // routing key (queue name)
+			false,  // mandatory
+			false,  // immediate
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        []byte(body),
+			},
+		)
+		failOnError(err, "Failed to publish a message")
+		log.Printf(" [x] Sent ID %s", body)
+		time.Sleep(1 * time.Second) // Add delay to simulate task distribution
+	}
 }
