be2e2102f44b13e99661794cf862b8280d109dc7
Shreypatel13ll <shrey1patel2@gmail.com>
2024-10-29 16:48:04 +0530

    test

---
+++ pkg/QueueHandler/queueHandler.go
@@ -0,0 +1,69 @@
+package queueHandler
+
+import (
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+var (
+	conn *amqp.Connection
+	ch   *amqp.Channel
+	q    amqp.Queue
+)
+
+// Helper function to handle errors
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+// Initialize RabbitMQ connection, channel, and declare a durable task queue
+func Init() {
+	var err error
+	conn, err = amqp.Dial("amqp://admin:ello@206.189.131.249:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+
+	ch, err = conn.Channel()
+	failOnError(err, "Failed to open a channel")
+
+	// Declare a durable task queue
+	q, err = ch.QueueDeclare(
+		"task_queue", // name of the queue
+		true,         // durable
+		false,        // delete when unused
+		false,        // exclusive
+		false,        // no-wait
+		nil,          // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+}
+
+// AddTask is a global function to add tasks (IDs) to the queue
+func AddTask(taskID string) {
+
+	err := ch.Publish(
+		"",     // exchange
+		q.Name, // routing key (queue name)
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			DeliveryMode: amqp.Persistent, // Make message persistent
+			ContentType:  "text/plain",
+			Body:         []byte(taskID),
+		})
+	failOnError(err, "Failed to publish a message")
+
+	log.Printf(" [x] Sent task ID: %d", taskID)
+}
+
+func Cleanup() {
+	// Close the channel and connection when no longer needed
+	if ch != nil {
+		ch.Close()
+	}
+	if conn != nil {
+		conn.Close()
+	}
+}
--- src/master/send.go
+++ src/master/send.go
@@ -1,55 +1,50 @@
 package main
 
-import (
-	"log"
-	"strconv"
-	"time"
+// amqp "github.com/rabbitmq/amqp091-go"
 
-	amqp "github.com/rabbitmq/amqp091-go"
-)
-
-func failOnError(err error, msg string) {
-	if err != nil {
-		log.Panicf("%s: %s", msg, err)
-	}
-}
+// func failOnError(err error, msg string) {
+// 	if err != nil {
+// 		log.Panicf("%s: %s", msg, err)
+// 	}
+// }
 
 func main() {
-	conn, err := amqp.Dial("amqp://admin:ello@206.189.131.249:5672/")
-	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
 
-	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
+	// conn, err := amqp.Dial("amqp://admin:ello@206.189.131.249:5672/")
+	// failOnError(err, "Failed to connect to RabbitMQ")
+	// defer conn.Close()
 
-	// Declare a queue for the tasks
-	q, err := ch.QueueDeclare(
-		"task_queue", // name of the queue
-		true,         // durable
-		false,        // delete when unused
-		false,        // exclusive
-		false,        // no-wait
-		nil,          // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
+	// ch, err := conn.Channel()
+	// failOnError(err, "Failed to open a channel")
+	// defer ch.Close()
 
-	// Push some IDs into the queue as tasks
-	for i := 1; i <= 10; i++ {
-		body := strconv.Itoa(i) // Convert the ID to string
-		err = ch.Publish(
-			"",     // exchange
-			q.Name, // routing key (queue name)
-			false,  // mandatory
-			false,  // immediate
-			amqp.Publishing{
-				DeliveryMode: amqp.Persistent, // Make message persistent
-				ContentType:  "text/plain",
-				Body:         []byte(body),
-			},
-		)
-		failOnError(err, "Failed to publish a message")
-		log.Printf(" [x] Sent ID %s", body)
-		time.Sleep(1 * time.Second) // Add delay to simulate task distribution
-	}
+	// // Declare a queue for the tasks
+	// q, err := ch.QueueDeclare(
+	// 	"task_queue", // name of the queue
+	// 	true,         // durable
+	// 	false,        // delete when unused
+	// 	false,        // exclusive
+	// 	false,        // no-wait
+	// 	nil,          // arguments
+	// )
+	// failOnError(err, "Failed to declare a queue")
+
+	// // Push some IDs into the queue as tasks
+	// for i := 1; i <= 10; i++ {
+	// 	body := strconv.Itoa(i) // Convert the ID to string
+	// 	err = ch.Publish(
+	// 		"",     // exchange
+	// 		q.Name, // routing key (queue name)
+	// 		false,  // mandatory
+	// 		false,  // immediate
+	// 		amqp.Publishing{
+	// 			DeliveryMode: amqp.Persistent, // Make message persistent
+	// 			ContentType:  "text/plain",
+	// 			Body:         []byte(body),
+	// 		},
+	// 	)
+	// 	failOnError(err, "Failed to publish a message")
+	// 	log.Printf(" [x] Sent ID %s", body)
+	// 	time.Sleep(1 * time.Second) // Add delay to simulate task distribution
+	// }
 }
